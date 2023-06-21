68decc9173760440a5d9530aae8605b1013aa73f
imnayandave <imnayandave@gmail.com>
2023-06-21 21:54:34 +0530

    Initial Commit

---
+++ Demo_WeeklyCalendar/Demo_WeeklyCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_WeeklyCalendarApp.swift
+//  Demo_WeeklyCalendar
+//
+//  Created by Nayan Dave on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_WeeklyCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
